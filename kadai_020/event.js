c3c1b4792c6e8059f06f434b13170c08efaac279
chiharu715 <154907431+chiharu715@users.noreply.github.com>
2024-01-22 19:39:30 +0900

    Add files via upload

---
+++ kadai_020/event.js
@@ -0,0 +1,9 @@
+
+const textElement = document.getElementById('text');
+
+const buttonElement = document.getElementById('btn');
+
+btn.addEventListener('click',() => {
+ textElement.textContent = 'ボタンをクリックしました'});
+
+
